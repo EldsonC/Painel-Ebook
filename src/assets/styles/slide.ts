8ab3288dcd497a718a1abc17d6c8d8587b0f92a6
Eldson Caldas <81312380+EldsonC@users.noreply.github.com>
2023-06-01 21:32:00 -0300

    mobile-slide

--- src/assets/styles/slide.ts
+++ src/assets/styles/slide.ts
@@ -60,51 +60,62 @@ export const SlideStyle = styled.div `
     }
 
     @media screen and (max-width: 900px){
-        padding: 0 15px;
-        margin-top: 15px;
-        width: calc(100vw - 30px);
+        padding: 0 20px;
+        margin-top: 47px;
+        width: calc(100vw - 40px);
 
         .slide {
             width: 100%;
             display: flex;
-            justify-content: center;
 
-            gap: 31px;
-            
+            justify-content: space-between;
             border-bottom: 1px solid #D9D9D9;
 
-            button {
-                padding: 13px 0px;
-
-                border: none;
-                outline: none;
-
-                background: transparent;
-
-                font-size: 16px;
-                font-weight: 500;
-                color: #303642;
-
-                cursor: pointer;
-            }
-
-            #first-btn {
-                margin-left: 0px;
-                font-weight: 700;
-
-                ::before {
-                    content: "";
-                    display: flex;
-
-                    width: 100%;
-                    height: 4px;
-                    
-                    position: relative;
-                    bottom: -33px;
-
-                    background: #303642;
+            .container-actions {
+                display: flex;
+                gap: 31px;
+                button {
+                    padding: 13px 0px;
+        
+                    border: none;
+                    outline: none;
+        
+                    background: transparent;
+        
+                    font-size: 16px;
+                    font-weight: 500;
+                    color: #303642;
+        
+                    cursor: pointer;
+                }
+        
+                #first-btn {
+                    margin-left: 21px;
+                    font-weight: 700;
+        
+                    ::before {
+                        content: "";
+                        display: flex;
+        
+                        width: 100%;
+                        height: 4px;
+                        
+                        position: relative;
+                        bottom: -33px;
+        
+                        background: #303642;
+                    }
                 }
             }
+
+            .total-books {
+                span {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: #8a8a8a;
+                }
+            }
+
         }
     }
 `;
