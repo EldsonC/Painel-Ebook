d729a01dbeed380be9f68bfcf703eebda53839ad
Eldson Caldas <81312380+EldsonC@users.noreply.github.com>
2023-06-19 12:49:36 -0300

    api-netlify

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://back-sete.herokuapp.com/"
+    baseURL: "https://main--candid-gecko-b85afc.netlify.app/.netlify/functions/server"
 })
