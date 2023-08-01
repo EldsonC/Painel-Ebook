6c6f4e6c675ebbdfa3d61b1fc482371382d8ef04
Eldson Caldas <81312380+EldsonC@users.noreply.github.com>
2023-08-01 16:25:41 +0000

    Api

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://main--candid-gecko-b85afc.netlify.app/.netlify/functions/server"
+    baseURL: "u719074247_ebook"
 })
